db98e27c27fa3c32ce3bb080f8b24330a5e257b3
Tommy Bidne <tbidne@protonmail.com>
2022-10-27 12:42:03 +1300

    Tasty provider for scripts

---
+++ src/Plutarch/Test/Script.hs
@@ -0,0 +1,148 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+{- | Tasty provider for testing 'ScriptCase'.
+
+ @since 1.3
+-}
+module Plutarch.Test.Script (
+  ScriptCase (..),
+  ScriptResult (..),
+  testScript,
+) where
+
+import GHC.Generics (Generic)
+
+--------------------------------------------------------------------------------
+
+import Data.Tagged (Tagged (Tagged))
+import Data.Text (Text)
+import Data.Text qualified as T
+import Optics.Core (view)
+import Optics.TH (makeFieldLabelsNoPrefix, makePrisms)
+import Test.Tasty.Providers (
+  IsTest (
+    run,
+    testOptions
+  ),
+  TestTree,
+  singleTest,
+  testFailed,
+  testPassed,
+ )
+
+--------------------------------------------------------------------------------
+
+import Plutarch.Evaluate (evalScript)
+import PlutusLedgerApi.V2 (
+  Script,
+ )
+
+--------------------------------------------------------------------------------
+
+-- | @since 1.3
+data ScriptResult
+  = -- | @since 1.3
+    ScriptSuccess
+  | -- | @since 1.3
+    ScriptFailure
+  deriving stock
+    ( -- | @since 1.3
+      Eq
+    , -- | @since 1.3
+      Show
+    )
+
+-- | @since 1.3
+makePrisms ''ScriptResult
+
+{- | Full script info for testing.
+
+ @since 1.3
+-}
+data ScriptCase = ScriptCase
+  { expectation :: ScriptResult
+  -- ^ The expectation.
+  --
+  -- @since 1.3
+  , script :: Script
+  -- ^ The script.
+  --
+  -- @since 1.3
+  , debugScript :: Script
+  -- ^ Debug version of the script for .
+  --
+  -- @since 1.3
+  }
+  deriving stock
+    ( -- | @since 1.3
+      Eq
+    , -- | @since 1.3
+      Generic
+    , -- | @since 1.3
+      Show
+    )
+
+-- | @since 1.3
+makeFieldLabelsNoPrefix ''ScriptCase
+
+-- | @since 1.3
+instance IsTest ScriptCase where
+  testOptions = Tagged []
+  run _options sc _progress = do
+    case (view #expectation sc, runScriptCase sc) of
+      -- expected success, received failure
+      (ScriptSuccess, (ScriptFailure, msg)) -> pure $ testFailed msg
+      -- expected failure, received success
+      (ScriptFailure, (ScriptSuccess, msg)) -> pure $ testFailed msg
+      _ -> pure $ testPassed ""
+
+{- | Convenience alias for 'singleTest'.
+
+ @since 1.3
+-}
+testScript :: String -> ScriptCase -> TestTree
+testScript = singleTest
+
+{- | Low-level function for running a 'ScriptCase'.
+
+ @since 1.3
+-}
+runScriptCase :: ScriptCase -> (ScriptResult, String)
+runScriptCase sc =
+  runScript
+    (view #script sc)
+    (view #debugScript sc)
+    msg
+  where
+    msg = case view #expectation sc of
+      ScriptSuccess -> ""
+      ScriptFailure -> "Expected failure, but script succeeded"
+
+{- | Low-level function for running a script.
+
+ @since 1.3
+-}
+runScript ::
+  -- | Script to run.
+  Script ->
+  -- | Debug version of the script.
+  Script ->
+  -- | Message to return upon success.
+  String ->
+  -- | Returns the result of evaluating the script, along with the parameter
+  -- message upon success, or an error message upon failure.
+  (ScriptResult, String)
+runScript script debug onSuccess = case scriptResult of
+  (Right _, _, _) -> (ScriptSuccess, onSuccess)
+  (Left err, _, _) -> (ScriptFailure, showError dTrace (show err))
+  where
+    scriptResult = evalScript script
+    (_, _, dTrace) = evalScript debug
+
+showError :: [Text] -> String -> String
+showError traces err =
+  "Script failed with error: "
+    <> err
+    <> "\nTrace Log:\n"
+    <> T.unpack (T.intercalate "\n" traces)
